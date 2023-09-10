005b3c3d6bdf26aea23eae2cdf5040fe1926aa5e
alexskvortsov <sky0210@yandex.ru>
2023-09-10 11:51:22 +0300

    ase12345

--- ase.h
+++ ase.h
@@ -3,3 +3,26 @@ sfsmdfl;ms;'df;
 fks'dfsfdls;df;lsfd
 smdfsmd;fm'sdf;
 fsmd;f's';df;
+
+fgldsfg'sdf
+gzfmg'zmlfgmzf
+zfg;zmgfz;
+fg
+bzxfm;bzbm;zxb
+
+gdfmz
+fgzd
+fg
+zgkszfgdsfgnzdld'fgz'f
+ g
+ zjg
+ zj
+ gfz
+ g
+
+ jfjz
+ jsogjz
+ fgjz
+ gjo
+ z
+ gjzjghz
