2393f8208daeaa70cabe21dd228dc935121ddbb3
alexskvortsov <sky0210@yandex.ru>
2023-09-10 11:58:26 +0300

    gdknglsdngsdngfjsdfgsdlgf

--- hes.h
+++ hes.h
@@ -2,3 +2,10 @@ int a;
 int b;
 int c;
 int d;
+
+fnsadfnasdfaskdfnasdgna;lsdg
+gangang;agajlgbal;signeddnsgnanlkgnal
+sdgnasdgna;gdn;andaddgnnlasnlan;gd
+v va;ldgla;sdgn;adg
+avnangagnadg
+agnangdlang'a'ga
